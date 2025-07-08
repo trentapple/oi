2594bb9ebc20d184dd69ae235a58859b1941e1ce
Trent Apple <trentapple@users.noreply.github.com>
2025-07-07 23:25:06 -0500

    Update env.py

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -107,10 +107,12 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+if WEBUI_NAME != "OI":
+    if WEBUI_NAME != "Open WebUI":
+        WEBUI_NAME += " (Open WebUI)"
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+# Fallback to secure remote URL
+WEBUI_FAVICON_URL = os.environ.get("WEBUI_FAVICON_URL", "https://openwebui.com/favicon.png")
 
 TRUSTED_SIGNATURE_KEY = os.environ.get("TRUSTED_SIGNATURE_KEY", "")
 
